e1628cf79354b1cea763c11a7f3b1aa5391f991f
Spencer Curran <s.curran@setonhill.edu>
2020-02-18 12:02:15 -0500

    thank you !

--- In Class Alerts/ViewController.swift
+++ In Class Alerts/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func display(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Harsh Reality!", message: "You're being monitored :)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:NSLocalizedString("accept this truth", comment: "default action"), style: .`default`, handler: { _ in}))
+        
+        self.present(alert,animated: true, completion: nil)
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
